a4d4d49fe73cd5feeee793fc5a4100fc82b62ea5
Ryan Chandler <ryangjchandler@gmail.com>
2021-09-02 17:26:29 +0100

    Initial commit

---
+++ src/Example.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NunoMaduro\SkeletonPhp;
+
+/**
+ * @internal
+ */
+final class Example
+{
+    public function foo(): string
+    {
+        return 'bar';
+    }
+}
